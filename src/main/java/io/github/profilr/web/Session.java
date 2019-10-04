0cad892a3c68b2939453def04600d0fb2adba291
Arjun Vikram <arjvik@gmail.com>
2019-10-04 12:27:23 -0500

    Don't persist virtual sessions unless needed

--- src/main/java/io/github/profilr/web/Session.java
+++ src/main/java/io/github/profilr/web/Session.java
@@ -15,25 +15,59 @@ import io.github.profilr.domain.User;
 
 public class Session implements Map<String, Object> {
 
-	private HttpSession session;
+	private HttpServletRequest request;
 
+	/**
+	 * For dependency injection purposes only.
+	 * Do not remove this constructor!
+	 * HK2 fails to inject a proxy of this class 
+	 * into an ExceptionMapper unless there is
+	 * an additional no-arg constructor
+	 */
 	protected Session() {
-		// For dependency injection purposes only.
-		// Do not remove this constructor!
-		// HK2 fails to inject a proxy of this class 
-		// into an ExceptionMapper unless there is
-		// an additional no-arg constructor
+		// do nothing
 	}
 	
 	@Inject
 	public Session(HttpServletRequest request) {
-		this.session = request.getSession();
+		this.request = request;
+	}
+	
+	/**
+	 * 
+	 * A virtual session is one which does not store any properties,
+	 * and thus has no reason to be stored. Calling the {@code request.getSession()}
+	 * method when a session is virtual will cause it to be created and persisted,
+	 * so we avoid doing so unless something needs to be stored in the current session.
+	 * 
+	 * @return true if there is no session for the current request,
+	 * 		   false if there is already a saved session
+	 */
+	public boolean isVirtual() {
+		return request.getSession(false) == null;
+	}
+	
+	/**
+	 * Creates a new saved session if the current session is virtual (does not exist)
+	 */
+	public void createSessionIfNotSaved() {
+		request.getSession(true);
+	}
+	
+	/**
+	 * @return a {@code HttpSession} object representing the current
+	 * 			session, null if it is virtual (does not exist)
+	 */
+	private HttpSession getSavedSession() {
+		return request.getSession(false);
 	}
 	
 	@Override
 	public int size() {
+		if (isVirtual())
+			return 0;
 		int i = 0;
-		Enumeration<?> e = session.getAttributeNames();
+		Enumeration<?> e = getSavedSession().getAttributeNames();
 		while(e.hasMoreElements()) {
 			e.nextElement();
 			i++;
@@ -43,12 +77,12 @@ public class Session implements Map<String, Object> {
 
 	@Override
 	public boolean isEmpty() {
-		return !session.getAttributeNames().hasMoreElements();
+		return isVirtual() || !getSavedSession().getAttributeNames().hasMoreElements();
 	}
 
 	@Override
 	public boolean containsKey(Object key) {
-		return session.getAttribute((String) key)!=null;
+		return !isVirtual() && getSavedSession().getAttribute((String) key) != null;
 	}
 
 	@Override
@@ -58,21 +92,24 @@ public class Session implements Map<String, Object> {
 
 	@Override
 	public Object get(Object key) {
-		return session.getAttribute((String) key);
+		return isVirtual() ? null : getSavedSession().getAttribute((String) key);
 	}
 
 	@Override
 	public Object put(String key, Object value) {
+		createSessionIfNotSaved();
 		if(value==null)
 			throw new NullPointerException();
-		session.setAttribute(key, value);
+		getSavedSession().setAttribute(key, value);
 		return null;
 	}
 
 	@Override
 	public Object remove(Object key) {
-		Object value = session.getAttribute((String) key);
-		session.removeAttribute((String) key);
+		if (isVirtual())
+			return null;
+		Object value = getSavedSession().getAttribute((String) key);
+		getSavedSession().removeAttribute((String) key);
 		return value;
 	}
 
@@ -84,15 +121,19 @@ public class Session implements Map<String, Object> {
 
 	@Override
 	public void clear() {
-		Enumeration<?> e = session.getAttributeNames();
+		if (isVirtual())
+			return;
+		Enumeration<?> e = getSavedSession().getAttributeNames();
 		while(e.hasMoreElements())
 			remove(e.nextElement());
 	}
 
 	@Override
 	public Set<String> keySet() {
+		if (isVirtual())
+			return new HashSet<>();
 		Set<String> s = new HashSet<>();
-		Enumeration<?> e = session.getAttributeNames();
+		Enumeration<?> e = getSavedSession().getAttributeNames();
 		while(e.hasMoreElements())
 			s.add((String) e.nextElement());
 		return Collections.unmodifiableSet(s);
@@ -100,25 +141,29 @@ public class Session implements Map<String, Object> {
 
 	@Override
 	public Collection<Object> values() {
+		if (isVirtual())
+			return new HashSet<>();
 		Set<Object> s = new HashSet<>();
-		Enumeration<?> e = session.getAttributeNames();
+		Enumeration<?> e = getSavedSession().getAttributeNames();
 		while(e.hasMoreElements())
-			s.add(session.getAttribute((String) e.nextElement()));
+			s.add(getSavedSession().getAttribute((String) e.nextElement()));
 		return Collections.unmodifiableSet(s);
 	}
 
 	@Override
 	public Set<java.util.Map.Entry<String, Object>> entrySet() {
+		if (isVirtual())
+			return new HashSet<>();
 		Set<Entry<String,Object>> s = new HashSet<>();
-		Enumeration<?> e = session.getAttributeNames();
+		Enumeration<?> e = getSavedSession().getAttributeNames();
 		while(e.hasMoreElements()) {
 			String key = (String) e.nextElement();
-			s.add(new AbstractMap.SimpleImmutableEntry<String,Object>(key, session.getAttribute(key)));
+			s.add(new AbstractMap.SimpleImmutableEntry<String,Object>(key, getSavedSession().getAttribute(key)));
 		}
 		return Collections.unmodifiableSet(s);
 	}
 	
-	/** same as <code>((User) get("user))</code> convenience method because we kept doing this */
+	/** same as <code>((User) get("user"))</code> convenience method because we kept doing this */
 	public User getUser() {
 		return (User) get("user");
 	}
