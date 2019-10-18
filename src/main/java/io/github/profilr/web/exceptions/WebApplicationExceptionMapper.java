c38145127bee7626c9c37da70c104541693ac524
Arjun Vikram <arjvik@gmail.com>
2019-10-18 11:48:25 -0500

    Let WebApplicationExceptions be handled on their own

---
+++ src/main/java/io/github/profilr/web/exceptions/WebApplicationExceptionMapper.java
@@ -0,0 +1,16 @@
+package io.github.profilr.web.exceptions;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+@Provider
+public class WebApplicationExceptionMapper implements ExceptionMapper<WebApplicationException> {
+
+	@Override
+	public Response toResponse(WebApplicationException e) {
+		return e.getResponse();
+	}
+
+}
